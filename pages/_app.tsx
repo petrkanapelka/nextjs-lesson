dcf48c9462fd0b7470f8550eb1af73e97cc21f65
Petr Kanapelka <kanapelka.petr@gmail.com>
2025-01-15 20:22:33 +0300

    feat: add RTK query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import type {AppProps} from 'next/app';
-import {ReactElement, ReactNode} from 'react';
-import {NextPage} from 'next';
+import type { AppProps } from 'next/app';
+import { ReactElement, ReactNode, useState } from 'react';
+import { NextPage } from 'next';
+import { Hydrate, QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 export type NextPageWithLayout<P = {}> = NextPage<P> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -11,7 +12,15 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  const [queryClient] = useState(() => new QueryClient)
+
   const getLayout = Component.getLayout ?? ((page) => page);
 
-  return getLayout(<Component {...pageProps} />);
+  return getLayout(
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydrateProps}>
+        <Component {...pageProps} />
+      </Hydrate>
+    </QueryClientProvider>
+  );
 }
