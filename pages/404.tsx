003efa81813d8ddde6c2d6bf6a0fe27ab569e47b
Petr Kanapelka <kanapelka.petr@gmail.com>
2025-01-16 21:10:06 +0300

    feat: create 404 component

---
+++ pages/404.tsx
@@ -0,0 +1,45 @@
+// pages/404.tsx
+import { getLayout } from 'components/Layout/BaseLayout/BaseLayout';
+import { PageWrapper } from 'components/PageWrapper/PageWrapper';
+import Link from 'next/link';
+import { CSSProperties } from 'react';
+
+const Custom404 = () => {
+    return (
+        <PageWrapper>
+            <div style={styles.container}>
+                <h1 style={styles.heading}>404 - Page Not Found</h1>
+                <p style={styles.text}>
+                    Sorry, the page you are looking for does not exist. Go back to{' '}
+                    <Link href="/" style={styles.link}>
+                        Home
+                    </Link>
+                    .
+                </p>
+            </div>
+        </PageWrapper>
+    );
+};
+
+const styles: { [key: string]: CSSProperties } = {
+    container: {
+        textAlign: 'center',
+        padding: '50px',
+        fontFamily: 'Arial, sans-serif',
+    },
+    heading: {
+        fontSize: '48px',
+        margin: '0 0 20px',
+    },
+    text: {
+        fontSize: '18px',
+    },
+    link: {
+        color: '#0070f3',
+        textDecoration: 'underline',
+    },
+};
+
+Custom404.getLayout = getLayout
+
+export default Custom404;
