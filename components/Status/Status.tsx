6bd6d5386b0c233bb8610234db94aff31a23e075
Petr Kanapelka <kanapelka.petr@gmail.com>
2025-01-16 21:10:42 +0300

    feat: create CharacterCard

---
+++ components/Status/Status.tsx
@@ -0,0 +1,15 @@
+import { CharacterStatusType } from 'assets/api/rick-and-morty-api';
+import Image, { StaticImageData } from 'next/image';
+type Props = {
+    status: CharacterStatusType
+    src: StaticImageData
+};
+export const Status = (props: Props) => {
+
+    const { status, src } = props;
+    return (
+        <>
+            <Image src={src} width={20} height={20} alt='status' />
+        </>
+    );
+};
