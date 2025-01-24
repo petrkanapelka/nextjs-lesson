ce8eaaefbaa114ecd114e9a160fab0a44943f27c
Petr Kanapelka <kanapelka.petr@gmail.com>
2025-01-24 15:20:33 +0300

    feat: enhance server-side caching for episodes page

--- pages/episodes/index.tsx
+++ pages/episodes/index.tsx
@@ -3,8 +3,13 @@ import { EpisodeType, ResponseType } from "assets/api/rick-and-morty-api";
 import { Card } from "components/Card/Card";
 import { getLayout } from "components/Layout/BaseLayout/BaseLayout";
 import { PageWrapper } from "components/PageWrapper/PageWrapper";
+import { GetServerSideProps } from "next";
 
-export const getServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+
+    res.setHeader('Cache-Control', 'public, s-maxage=10, stale-while-revalidate=100')
+
+
     const episodes = await API.rickAndMorty.getEpisodes()
 
     if (!episodes) {
