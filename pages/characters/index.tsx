fbf4511844689febfbfcb6dca5f2d6dcd809566a
Petr Kanapelka <kanapelka.petr@gmail.com>
2025-01-24 16:16:05 +0300

    feat: dynamically import CharacterCard component for improved performance

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,9 +1,12 @@
 import { API } from "../../assets/api/api";
 import { CharacterType, ResponseType } from "../../assets/api/rick-and-morty-api";
-import { CharacterCard } from "../../components/Card/CharacterCard/CharacterCard";
-import { Header } from "../../components/Header/Header";
 import { PageWrapper } from "../../components/PageWrapper/PageWrapper";
 import { getLayout } from '../../components/Layout/BaseLayout/BaseLayout';
+import dynamic from "next/dynamic";
+
+const CharacterCard = dynamic(() => import('../../components/Card/CharacterCard/CharacterCard')
+    .then((mod) => mod.CharacterCard), { ssr: false }
+);
 
 
 export const getStaticProps = async () => {
