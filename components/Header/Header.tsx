c8b414d4ee7aafd3de5646d1a917c1107c6cb03e
Petr Kanapelka <kanapelka.petr@gmail.com>
2025-01-26 00:01:14 +0300

    feat: add 'Private' link to Header component for improved navigation

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,6 +7,7 @@ export const Header = () => (
     <LinkBlock title={'Locations'} />
     <LinkBlock title={'Episodes'} />
     <LinkBlock title={'Testing'} />
+    <LinkBlock title={'Private'} />
   </Navbar>
 )
 
