a112cc52d77fb75418657a4299e03ee35cd68cb4
Petr Kanapelka <kanapelka.petr@gmail.com>
2025-01-26 01:04:45 +0300

    feat: add "use client" directive to PageWrapper component for client-side rendering

--- components/PageWrapper/PageWrapper.tsx
+++ components/PageWrapper/PageWrapper.tsx
@@ -1,6 +1,8 @@
+"use client"
+
 import { PropsWithChildren } from "react";
 import styled from "styled-components";
-import {HeadMeta} from '../HeadMeta/HeadMeta';
+import { HeadMeta } from '../HeadMeta/HeadMeta';
 
 type PropsType = {
   title?: string;
