d902fd5d5af1947f54b3a070eaed934e7e3e10cc
Petr Kanapelka <kanapelka.petr@gmail.com>
2025-01-26 01:03:26 +0300

    feat: enable experimental app directory support in Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,16 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'rickandmortyapi.com',
-        port: '',
-        pathname: '/api/character/avatar/**',
-      },
-    ]
-  }
-}
+    reactStrictMode: true,
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'rickandmortyapi.com',
+                port: '',
+                pathname: '/api/character/avatar/**',
+            },
+        ],
+    },
+    experimental: {
+        appDir: true,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
