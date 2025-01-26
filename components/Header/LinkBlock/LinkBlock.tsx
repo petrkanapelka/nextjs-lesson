02cac2f6eeba98b6727b485e564d6120e06a1fd2
Petr Kanapelka <kanapelka.petr@gmail.com>
2025-01-27 00:20:37 +0300

    refactor: replace styled-components with CSS module in LinkBlock component

--- components/Header/LinkBlock/LinkBlock.tsx
+++ components/Header/LinkBlock/LinkBlock.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import styled from "styled-components";
+import React from "react";
+import "./LinkBlock.module.css";
 
 type PropsType = {
   title: string;
@@ -9,31 +10,10 @@ export const LinkBlock = (props: PropsType) => {
   const { title } = props;
 
   return (
-    <LinkWrapper>
+    <div className="link-wrapper">
       <Link href={`/${title.toLowerCase()}`}>
         <h2>{title} →</h2>
       </Link>
-    </LinkWrapper>
+    </div>
   );
 };
-
-const LinkWrapper = styled.div`
-  padding: 20px;
-  border-radius: 12px;
-  border: 1px solid rgba(131, 134, 135, 0);
-
-  & h2 {
-    font-weight: 600;
-    font-size: 28px;
-    margin-bottom: 0.7rem;
-  }
-
-  @media (max-width: 700px) {
-    padding: 4px;
-
-    & h2 {
-      font-weight: 600;
-      font-size: 24px;
-    }
-  }
-`;
