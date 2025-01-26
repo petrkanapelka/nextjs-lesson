492b1593559ef478430d69f791c58cdb70c51548
Petr Kanapelka <kanapelka.petr@gmail.com>
2025-01-27 00:18:15 +0300

    feat: prepare layout for Header component integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,5 @@
+import { Header } from "components/Header/Header";
+
 export default function RootLayout({
     children,
 }: {
@@ -5,7 +7,10 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en">
-            <body>{children}</body>
+            <body>
+                {/* <Header /> */}
+                {children}
+            </body>
         </html>
     );
 }
